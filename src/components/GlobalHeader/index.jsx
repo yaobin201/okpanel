8f4b6ad9d055ace26157fded80c1ff530fe0871a
YAOBIN <1501209698@qq.com>
2025-03-14 11:14:57 +0800

    修改网站内容

--- src/components/GlobalHeader/index.jsx
+++ src/components/GlobalHeader/index.jsx
@@ -49,6 +49,14 @@ export default function GlobalHeader() {
                         {item.cat}
                       </a>
                     ))}
+                    <a
+                        href="https://daydayupai.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                      >
+                        DayUpAi
+                      </a>
                   </div>
                 </div>
               </div>
@@ -84,6 +92,15 @@ export default function GlobalHeader() {
                   {item.cat}
                 </Disclosure.Button>
               ))}
+
+<a
+                        href="https://daydayupai.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
+                      >
+                        DayUpAi
+                      </a>
             </div>
           </Disclosure.Panel>
         </>
